445c09a1068aa6fba69e7d1a55632dfa48bbadd4
Hari Krishna <harikrishnacsse@gmail.com>
2021-07-18 17:47:45 +0530

    initial commit

---
+++ src/main/java/com/test/ubiquitousairplane/UbiquitousAirplaneApplication.java
@@ -0,0 +1,13 @@
+package com.test.ubiquitousairplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiquitousAirplaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbiquitousAirplaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/ubiquitousairplane/UbiquitousAirplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.ubiquitousairplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbiquitousAirplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
